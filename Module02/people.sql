4adfbd0e65e9ea8ad3bc6e4430490c9965a6d2e8
Aleksey <90387971+allo163@users.noreply.github.com>
2022-02-07 22:43:02 +0400

    Add files via upload

---
+++ Module02/people.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS "people" (
+    "Person" NOT NULL PRIMARY KEY,
+    "Region" NOT NULL
+);
+INSERT INTO "people" VALUES
+    ('Anna Andreadi','West'),
+    ('Chuck Magee','East'),
+    ('Kelly Williams','Central'),
+    ('Cassandra Brandow','South');
